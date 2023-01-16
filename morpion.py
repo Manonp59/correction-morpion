378556a857e614596ac925607f4d89e18b6f79e2
jadamin <jadaminpro@gmail.com>
2023-01-16 13:55:43 +0100

    est pleine func

--- morpion.py
+++ morpion.py
@@ -74,3 +74,26 @@ def est_gagnante(plateau:dict) -> bool:
             return True
     
     return False
+
+def est_pleine(plateau:dict) -> bool:
+    """Fonction qui permet de savoir si la grille est pleine
+    Args:
+        plateau (dict): Un plateau de jeu
+    Returns:
+        bool: True si la grille est pleine, False sinon
+    """
+    
+    for cle in plateau:
+        for case in plateau[cle]:
+            if case == None:
+                return False
+    return True
+
+plateau = {
+    "A" : [None for _ in range(3)],
+    "B" : [None for _ in range(3)],
+    "C" : [None for _ in range(3)]
+}
+
+termine = False
+joueur = "X"
